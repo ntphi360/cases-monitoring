e86476ed6885acb5a4c67e463920c08b7f32b000
Phi Nguyen <ntphi360@gmail.com>
2026-08-04 23:56:50 +0700

    test api

---
+++ src/HoSoMonitoring.Api/Controllers/CasesController.cs
@@ -0,0 +1,25 @@
+﻿using HoSoMonitoring.Core.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HoSoMonitoring.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CasesController : ControllerBase
+    {
+        private readonly ICaseRepository _caseRepository;
+
+        public CasesController(ICaseRepository caseRepository)
+        {
+            _caseRepository = caseRepository;
+        }
+
+        [HttpGet("overdue")]
+        public async Task<IActionResult> GetOverdueCases([FromQuery] int count = 10)
+        {
+            var cases = await _caseRepository.GetOverdueCasesAsync(count);
+
+            return Ok(cases);
+        }
+    }
+}
--- src/HoSoMonitoring.Data/Repositories/CaseRepository.cs
+++ src/HoSoMonitoring.Data/Repositories/CaseRepository.cs
@@ -1,11 +1,27 @@
-﻿using HoSoMonitoring.Core.Repositories;
+﻿using HoSoMonitoring.Core.Content;
+using HoSoMonitoring.Core.Repositories;
+using HoSoMonitoring.Data.SeedWorks;
+using Microsoft.EntityFrameworkCore;
 
 namespace HoSoMonitoring.Data.Repositories
 {
-    public interface IUnitOfWork
+    public class CaseRepository
+        : RepositoryBase<Case, int>, ICaseRepository
     {
-        ICaseRepository Cases { get; }
+        public CaseRepository(HoSoMonitoringContext context)
+            : base(context)
+        {
+        }
 
-        Task<int> CompleteAsync();
+        public async Task<List<Case>> GetOverdueCasesAsync(int count)
+        {
+            return await _context.Cases
+                .Where(x =>
+                    x.Deadline < DateTime.Now &&
+                    x.CompletedAt == null)
+                .OrderBy(x => x.Deadline)
+                .Take(count)
+                .ToListAsync();
+        }
     }
 }
